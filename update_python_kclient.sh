5a56150862c6119e914650608314775fdfea87a2
Vadim <monstarnn@gmail.com>
2019-11-29 21:59:49 +0300

    update python lib docs

--- update_python_kclient.sh
+++ update_python_kclient.sh
@@ -3,7 +3,9 @@
 python3 prepare.py --source http://localhost:8082/docs/apidocs.json
 rm -r out/python
 java -jar swagger-codegen-cli.jar generate -c config.json -i prepared.json -l python -o out/python
-rm -r ../python-kclient/kclient/api ../python-kclient/kclient/models
+rm -r ../python-kclient/kclient/api ../python-kclient/kclient/models ../python-kclient/docs
 cp -r out/python/kclient/api ../python-kclient/kclient/api
 cp -r out/python/kclient/models ../python-kclient/kclient/models
 cp -r out/python/kclient/*.py ../python-kclient/kclient
+cp -r out/python/docs ../python-kclient/docs
+cp -r out/python/*.md ../python-kclient
